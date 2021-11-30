7a2598b00af092ed8fe9529e6ce34c705df6d101
Jparmo <jeffreyparmo@gmail.com>
2021-11-29 18:02:19 -0600

    calculadora boost

--- js/main.js
+++ js/main.js
@@ -1,57 +1,102 @@
-var operacionAnterior = "";
-var operacionActual = "";
+var operacionActual = 0;
 var calc = 0;
 var calc2 = 0;
-function pintar(tecla) {
-  var pantalla = document.getElementById("screen");
-  operacionActual = pantalla.innerText;
-  
-  if (tecla == 1) {
-    operacionActual = 1;
-  } else if (tecla == 2) {
-    operacionActual = 2;
-  } else if (tecla == 3) {
-    operacionActual = 3;
-  } else if (tecla == 4) {
-    operacionActual = 4;
-  } else if (tecla == 5) {
-    operacionActual = 5;
-  } else if (tecla == 6) {
-    operacionActual = 6;
-  } else if (tecla == 7) {
-    operacionActual = 7;
-  } else if (tecla == 8) {
-    operacionActual = 8;
-  } else if (tecla == 9) {
-    operacionActual = 9;
-  } else if (tecla == 0) {
-    operacionActual = 0;
-  } else if (tecla == ".") {
-    operacionActual = ".";
-  } else if (tecla == "+") {
-    calc = parseFloat(operacionActual);
-    borrar();
-    calc2 = parseFloat(operacionActual) 
+
+function displayScreen(num) {
+  if (operacionActual == 0) {
+    document.getElementById("screen").innerText = num;
+    operacionActual = num;
+  } else {
+    operacionActual = operacionActual.toString() + num.toString();
+    agregarnum();
   }
-  agregarnum();
-  //   return operacionActual;
 }
 
 function agregarnum() {
-  var pantalla = document.getElementById("screen");
-  operacionAnterior = operacionAnterior.toString() + operacionActual.toString();
-  pantalla.innerText = operacionAnterior;
+  document.getElementById("screen").innerText = operacionActual;
 }
 
 function borrar() {
-  var pantalla = document.getElementById("screen");
-  operacionActual = "";
-  operacionAnterior = "";
-  pantalla.innerText = operacionActual;
+  operacionActual = 0;
+  calc = 0;
+  agregarnum();
 }
 
+function deletLastNum() {
+  var pantalla = document.getElementById("screen");
+  pantalla.innerText = pantalla.innerText.slice(0, -1);
+  if (pantalla.innerText == "") {
+    borrar();
+  }
+}
 
-function sumar(num1, num2) {
-  total1 = parseFloat(num1) + parseFloat(num2);
-  return total1;
+// funcion previa para pintar en la pantalla
+// function pintar(tecla) {
+//   var pantalla = document.getElementById("screen");
+//   operacionActual = pantalla.innerText;
+
+//   if (tecla == 1) {
+//     operacionActual = 1;
+//   } else if (tecla == 2) {
+//     operacionActual = 2;
+//   } else if (tecla == 3) {
+//     operacionActual = 3;
+//   } else if (tecla == 4) {
+//     operacionActual = 4;
+//   } else if (tecla == 5) {
+//     operacionActual = 5;
+//   } else if (tecla == 6) {
+//     operacionActual = 6;
+//   } else if (tecla == 7) {
+//     operacionActual = 7;
+//   } else if (tecla == 8) {
+//     operacionActual = 8;
+//   } else if (tecla == 9) {
+//     operacionActual = 9;
+//   } else if (tecla == 0) {
+//     operacionActual = 0;
+//   } else if (tecla == ".") {
+//     operacionActual = ".";
+//   } else if (tecla == "+") {
+//     calc = parseFloat(operacionActual);
+//     borrar();
+
+//     calc2 = parseFloat(operacionActual)
+//   }
+//   agregarnum();
+//   //   return operacionActual;
+// }
+
+function operar(num) {
+  var num1 = parseFloat(operacionActual);
+  if (calc == 0) {
+    calc = num1;
+    agregarnum();
+  } else {
+    switch (num) {
+      case "+":
+        calc = calc + num1;
+        document.getElementById("screen").innerText = calc;
+        break;
+      case "-":
+        calc = calc - num1;
+        document.getElementById("screen").innerText = calc;
+        break;
+      case "/":
+        calc = calc / num1;
+        document.getElementById("screen").innerText = calc;
+        break;
+      case "*":
+        calc = calc * num1;
+        document.getElementById("screen").innerText = calc;
+        break;
+      default:
+        break;
+    }
+  }
+  operacionActual = 0;
+  console.log(num1);
+
+  // total1 = parseFloat(num1) + parseFloat(num2);
+  // return total1;
 }
